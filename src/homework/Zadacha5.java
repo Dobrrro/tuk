56d04deac99b732e0e8efdd18abc578cf3d28799
Dobrrro <Alex.D.Solovey@gmail.com>
2016-10-15 22:56:10 +0300

    5ая готова. БАБОЧКА

--- src/homework/Zadacha5.java
+++ src/homework/Zadacha5.java
@@ -1,6 +1,7 @@
 package homework;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 /**
@@ -8,35 +9,65 @@ import java.io.InputStreamReader;
  */
 public class Zadacha5
 {
-    public static void main(String[] args) throws Exception
-    {
-
+    public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        int a = Integer.parseInt(reader.readLine());
-        int b = a/2+1;
+        int [] mas = new int[Integer.parseInt(reader.readLine())];
+        int b = mas.length/2;
 
-        for (int i=1; i <= a; i++)
+        for (int i=1; i <=mas.length; i++)
         {
             if (i>b) continue;
-            for (int j = 1; j <= a; j++)
+            for (int j = 1; j <=mas.length; j++)
             {
-                if (j<=i || j>a-i) System.out.print("X");
-//                else if (j>a-i) System.out.print("X");
+                if (j<=i || j>mas.length-i) System.out.print("X");
                 else System.out.print(" ");
             }
             System.out.print("\n");
         }
-        for (int i=b+1; i <= a; i++)
+        for (int i=b+1; i <=mas.length; i++)
         {
-            for (int j = 1; j <= a; j++)
+            for (int j = 1; j <=mas.length; j++)
             {
                 if (j>=i) System.out.print("X");
-                else if (j<a-i+2) System.out.print("X");
+                else if (j<mas.length-i+2) System.out.print("X");
                 else System.out.print(" ");
             }
             System.out.print("\n");
         }
-
-
     }
 }
+
+
+//public class Zadacha5  //при помощи переменной:
+//{
+//    public static void main(String[] args) throws Exception
+//    {
+//
+//        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+//        int a = Integer.parseInt(reader.readLine());
+//        int b = a/2+1;
+//
+//        for (int i=1; i <= a; i++)
+//        {
+//            if (i>b) continue;
+//            for (int j = 1; j <= a; j++)
+//            {
+//                if (j<=i || j>a-i) System.out.print("X");
+//                else System.out.print(" ");
+//            }
+//            System.out.print("\n");
+//        }
+//        for (int i=b+1; i <= a; i++)
+//        {
+//            for (int j = 1; j <= a; j++)
+//            {
+//                if (j>=i) System.out.print("X");
+//                else if (j<a-i+2) System.out.print("X");
+//                else System.out.print(" ");
+//            }
+//            System.out.print("\n");
+//        }
+//
+//
+//    }
+//}
