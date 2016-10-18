d35162c533b906cf5acb4c36f95374dce25a8810
Dobrrro <Alex.D.Solovey@gmail.com>
2016-10-18 21:26:27 +0300

    8ая готова

---
+++ src/homework/Zadacha8.java
@@ -0,0 +1,10 @@
+package homework;
+
+import javax.xml.ws.Service;
+
+/**
+ * Created by Пыльный on 18.10.2016.
+ */
+ final class Zadacha8 {
+    private final Service[] services = new Service[10];
+}
