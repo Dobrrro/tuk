fe62366252d150ce4a6f6c314bd2a5dcc3e1648a
Dobrrro <Alex.D.Solovey@gmail.com>
2016-10-24 22:30:53 +0300

    6(5)ая готова.

---
+++ src/homework/Zadacha6_Delenie.java
@@ -0,0 +1,22 @@
+package homework;
+
+import static java.lang.StrictMath.pow;
+
+/**
+ * Created by Пыльный on 24.10.2016.
+ */
+
+
+//написать метод, который разделит одно число на другое не используя оператор деления. предусмотреть все возможные ошибки
+public class Zadacha6_Delenie
+    {
+
+        public static void main(String[] args)
+        {
+            System.out.println(del(72, 12));
+        }
+
+    private static double del(double a, double b) {
+        return a * pow(b, -1);
+    }
+}
